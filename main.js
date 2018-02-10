e99674dd5d38260495c2b2ce4b3fae07e32ecf31
inna golovataya <golovataya93@gmail.com>
2018-02-10 20:48:11 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,24 @@
+const alphabet = "qwertyuiop[]asdfghjkl;'zxcvbnm,./";
+
+function addKeyboardLayout(alphabet) {
+	let arrAlphabet = alphabet.split('');
+	let keyboardTop = arrAlphabet.filter((arr, i) => i >= arrAlphabet.indexOf("q") && i <= arrAlphabet.indexOf("]"));
+	let keyboardMid = arrAlphabet.filter((arr, i) => i >= arrAlphabet.indexOf("a") && i <= arrAlphabet.indexOf("'"));
+	let keyboardBot = arrAlphabet.filter((arr, i) => i >= arrAlphabet.indexOf("z") && i <= arrAlphabet.indexOf("/"));
+	let keyboard = [keyboardTop, keyboardMid, keyboardBot];
+	return keyboard;
+}
+
+function getRandCharInRow(row) {
+	let keyboard = addKeyboardLayout(alphabet);
+	let rowIndex = row - 1;
+	let elemIndex = Math.floor(Math.random() * keyboard[rowIndex].length);
+	return keyboard[rowIndex][elemIndex];
+}
+
+function getRandCharInAlph(keyboard) {
+	addKeyboardLayout(alphabet);
+	let rowIndex = Math.floor(Math.random() * keyboard.length) - 1;
+	let elemIndex = Math.floor(Math.random() * keyboard[rowIndex].length);
+	return keyboard[rowIndex][elemIndex];
+}
